e134eb322bdf6007cee3bb3b5356b1632d9ca630
Kamar5 <qfk51994@gmail.com>
2018-10-28 11:37:52 -0400

    Updates

---
+++ precache-manifest.cdfefe41aaa915dd142343b5c527370a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "d4a8fa328d78591a5cd9",
+    "url": "/calculator/static/js/main.d4a8fa32.chunk.js"
+  },
+  {
+    "revision": "b689ca2a1c210cc4865a",
+    "url": "/calculator/static/js/1.b689ca2a.chunk.js"
+  },
+  {
+    "revision": "d4a8fa328d78591a5cd9",
+    "url": "/calculator/static/css/main.689f9441.chunk.css"
+  },
+  {
+    "revision": "e8f66b6f8219bece317b1f7fedc1b5ef",
+    "url": "/calculator/index.html"
+  }
+];
